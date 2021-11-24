e510afbee29bf381b24e309f03dcbeedc060ea99
dror bahat <bahat.dror@gmail.com>
2021-11-24 18:19:11 +0200

    finished

--- index.js
+++ index.js
@@ -2,22 +2,17 @@ let outputElement = document.getElementById("output")
 let button = document.getElementById("runCodeButton")
 
 const runCode = () => {
-    getRandomNumber(0, 100)
-        .then((num) => {
-            outputElement.innerHTML = num + " is sababa"
-        })
-        .catch((num) => {
-            outputElement.innerHTML = num + " is basa"
-        })
+    doWork(currentTime)
 }
 
-const getRandomNumber = (min, max) => {
-    return new Promise((resolve, reject) => {
-        let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min
-        if (randomNumber % 2 === 0) {
-            resolve(randomNumber)
-        } else {
-            reject(randomNumber)
-        }
-    })
+const currentTime = () => {
+    let now = new Date()
+    let currentTimeString = now.toLocaleTimeString()
+    console.log(currentTimeString)
+}
+
+const doWork = (callback) => {
+    console.log("start")
+    callback()
+    console.log("end")
 }
